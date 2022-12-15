ff56d73baeaa84d5b59813ccd581e494d8387692
Ignacio Fonts <fontsignacio@gmail.com>
2022-12-15 17:32:23 -0300

    StatusModel and StatusScreen

---
+++ lib/models/status_model.dart
@@ -0,0 +1,37 @@
+import 'package:whatsapp_clone/models/chat_model.dart';
+
+class StatusModel {
+  final String imgUrl;
+  final String name; 
+  final String time;
+
+  StatusModel({required this.imgUrl,required this.name,required this.time});
+}
+
+List<StatusModel> status = [
+  StatusModel(
+    name: messageData[0].name,
+    time: "Today, 3:00AM",
+    imgUrl: messageData[0].imageUrl 
+  ),
+  StatusModel(
+    name: messageData[1].name,
+    time: "Today, 3:30AM",
+    imgUrl: messageData[1].imageUrl 
+  ),
+  StatusModel(
+    name: messageData[2].name,
+    time: "Today, 4:00AM",
+    imgUrl: messageData[2].imageUrl 
+  ),
+  StatusModel(
+    name: messageData[3].name,
+    time: "Today, 2:00AM",
+    imgUrl: messageData[3].imageUrl 
+  ),
+  StatusModel(
+    name: messageData[4].name,
+    time: "Today, 3:00AM",
+    imgUrl: messageData[4].imageUrl 
+  ),
+];
--- lib/pages/status.dart
+++ lib/pages/status.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/status_model.dart';
 
-class Status extends StatelessWidget {
+class Status extends StatefulWidget {
   const Status({super.key});
 
   @override
+  State<Status> createState() => _StatusState();
+}
+
+class _StatusState extends State<Status> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        Icon(Icons.photo_sharp,size: 170.0,color: Colors.blueAccent),
-        Text("Status")
-      ],
+  return ListView.builder(
+      itemCount: status.length,
+      itemBuilder:(context, i) => Column(
+        children: <Widget>[
+          const Divider(
+            height: 10.0,
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.green,
+              radius: 22.0,
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(status[i].imgUrl), 
+                radius: 20.0,               
+              )
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+              Text(status[i].name,
+              style: const TextStyle(fontWeight: FontWeight.bold),  
+            ),
+            ]),
+            subtitle: Container(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(status[i].time,
+              style: const TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+            ),
+          )
+        ]
+      ),
     );
   }
 }
